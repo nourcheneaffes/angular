4102459c3a9ff2112720d55a5849a7531af07531
nourchene123 <nourcheneaffes19@gmail.com>
2024-12-11 21:29:05 +0100

    Ajout de nouvelles fonctionnalités

--- src/app/appartement/add-appartment/add-appartment.component.ts
+++ src/app/appartement/add-appartment/add-appartment.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { Apartment } from 'src/app/core/models/appartement';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Residence } from 'src/app/core/models/residence';
 
 @Component({
@@ -10,58 +9,43 @@ import { Residence } from 'src/app/core/models/residence';
 })
 export class AddAppartmentComponent {
   apartForm: FormGroup;
-  newApart: Apartment = {  // Déclaration de la propriété newApart
-    apartNum: 0 ,
-    floorNum: 0,
-    surface: 0,
-    terrace: true,
-    surfaceterrace: 0,
-    category: '',
-    ResidenceId: 0
-  };
-  residences:Residence[]=[
-    {id:1,"name": "El fel","address":"Borj Cedria",
-    "image":"../../assets/images/R1.jpg", status: "Disponible"},
-    {id:2,"name": "El yasmine",
-    "address":"Ezzahra","image":"../../assets/images/R2.jpeg", status:
-    "Disponible" },
-    {id:3,"name": "El Arij",
-    "address":"Rades","image":"../../assets/images/R3.jpg", status:
-    "Vendu"},
-    {id:4,"name": "El Anber","address":"inconnu",
-    "image":"../../assets/images/R4.jpg", status: "En Construction"}
-    ];
+  residences: Residence[] = [
+    { id: 1, name: 'El Fel', address: 'Borj Cedria', image: '', status: 'Disponible' },
+    { id: 2, name: 'El Yasmine', address: 'Ezzahra', image: '', status: 'Disponible' },
+    { id: 3, name: 'El Arij', address: 'Rades', image: '', status: 'Vendu' },
+    { id: 4, name: 'El Anber', address: 'Inconnu', image: '', status: 'En Construction' }
+  ];
 
-    ///methode1 (formGroup + formControl)
-    aForm = new FormGroup({
-        apartNum: new FormControl('', [Validators.required, Validators.pattern('^[0-9]+$')]),
-        floorNum: new FormControl('', [Validators.required, Validators.pattern('^[0-9]+$')]),
-        surface: new FormControl('', Validators.required),
-        terrace: new FormControl('yes', Validators.required),
-        surfaceTerrace: new FormControl({ value: '', disabled: true }, Validators.required),
-        category: new FormControl('S+1', Validators.required),
-        residence: new FormControl('', Validators.required),
-      });
-
-      ///methode2 (formBuilder)
   constructor(private fb: FormBuilder) {
+    this.apartForm = this.fb.group({
+      apartNum: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
+      floorNum: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
+      surface: ['', Validators.required],
+      terrace: ['yes', Validators.required],
+      surfaceTerrace: [{ value: '', disabled: true }, Validators.pattern('^[0-9]+$')],
+      category: ['S+1', Validators.required],
+      residence: ['', Validators.required]
+    });
 
-  this.apartForm = this.fb.group({
-  apartNum: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
-    floorNum: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
-    surface: ['', Validators.required],
-    terrace: ['yes', Validators.required],
-    surfaceTerrace: [{ value: '', disabled: true }, Validators.required],
-    category: ['S+1', Validators.required],
-    residence: ['', Validators.required]
-  });
+    // Dynamically enable/disable surfaceTerrace based on terrace value
+    this.apartForm.get('terrace')?.valueChanges.subscribe((terraceValue) => {
+      const surfaceTerraceControl = this.apartForm.get('surfaceTerrace');
+      if (terraceValue === 'yes') {
+        surfaceTerraceControl?.enable();
+        surfaceTerraceControl?.setValidators([Validators.required, Validators.pattern('^[0-9]+$')]);
+      } else {
+        surfaceTerraceControl?.disable();
+        surfaceTerraceControl?.clearValidators();
+      }
+      surfaceTerraceControl?.updateValueAndValidity();
+    });
   }
 
   onAdd() {
     if (this.apartForm.valid) {
-      const newApart = this.apartForm.value;
-
-      console.log(newApart);
+      console.log('Apartment Data:', this.apartForm.value);
+    } else {
+      console.log('Form is invalid.');
     }
   }
 
@@ -71,5 +55,5 @@ export class AddAppartmentComponent {
       category: 'S+1'
     });
   }
-
+  
 }
